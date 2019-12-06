84b4a9a57e037c8f7df9435f4d585e95a76421b6
wangxiayun <wangxiayun@aikucun.com>
2019-12-06 21:18:11 +0800

    quick

--- src/main/java/com/wxy/learn/algorithm/sort/practice/MergeSort.java
+++ src/main/java/com/wxy/learn/algorithm/sort/practice/MergeSort.java
@@ -3,59 +3,56 @@ package com.wxy.learn.algorithm.sort.practice;
 import java.util.Arrays;
 
 public class MergeSort {
-    public static void mergeSort(int[] arr, int r) {
-        division(arr, 0, r - 1);
+
+    public static void mergeSort(int[] a, int n) {
+        mergeSortInternally(a, 0, n - 1);
     }
 
-    private static void division(int[] arr, int p, int r) {
+    private static void mergeSortInternally(int[] a, int p, int r) {
         if (p >= r) {
             return;
         }
-
         int q = p + (r - p) / 2;
+        /**
+         * 左边排序
+         */
+        mergeSortInternally(a, p, q);
+        /**
+         * 右边边排序
+         */
+        mergeSortInternally(a, q + 1, r);
 
-        division(arr, p, q);
-
-        division(arr, q + 1, r);
-
-        merge(arr, p, q, r);
+        merge(a, p, q, r);
     }
 
-    private static void merge(int[] arr, int p, int q, int r) {
-        int i = p;
-        int j = q + 1;
-        int k = 0;
+    private static void merge(int[] a, int p, int q, int r) {
+        int low = p;
+        int high = q + 1;
         int[] tmp = new int[r - p + 1];
-        while (i <= q && j <= r) {
-            if (arr[i] <= arr[j]) {
-                tmp[k++] = arr[i++];
-            } else {
-                tmp[k++] = arr[j++];
+        int k = 0;
+        while (low <= q && high <= r) {
+            if (a[low] <= a[high]) {
+                tmp[k++] = a[low ++];
+            }else {
+                tmp[k++] = a[high ++];
             }
         }
 
-        int start = i;
-        int end = q;
-
-        if (j <= r) {
-            start = j;
-            end = r;
+        while (low<=q){
+            tmp[k++] = a[low ++];
+        }
+        while (high<=r){
+            tmp[k++] = a[high ++];
         }
 
-        while (start <= end) {
-            tmp[k++] = arr[start++];
-        }
-
-        for (i = 0; i <= r - p; ++i) {
-            arr[p + i] = tmp[i];
+        for (int i=0;i<tmp.length;i++){
+            a[p+i] = tmp[i];
         }
     }
 
-
-
     public static void main(String[] args) {
-        int[] array = new int[]{6, 5, 4, 3, 2, 1};
-        mergeSort(array, array.length);
-        System.out.println(Arrays.toString(array));
+        int [] a = {6,5,4,3,2,1};
+        mergeSort(a,a.length);
+        System.out.println(Arrays.toString(a));
     }
 }
--- src/main/java/com/wxy/learn/algorithm/sort/practice/QuickSort.java
+++ src/main/java/com/wxy/learn/algorithm/sort/practice/QuickSort.java
@@ -1,4 +1,35 @@
-package com.wxy.learn.algorithm.sort.practice;
-
-public class QuickSort {
-}
+//package com.wxy.learn.algorithm.sort.practice;
+//
+//public class QuickSort {
+//    public static void quickSort(int[] a, int n) {
+//        quickSortInternally(a, 0, n - 1);
+//    }
+//
+//    public static void quickSortInternally(int[] a, int low, int high) {
+//        if (low >= high) {
+//            return;
+//        }
+//        // 获取分区点
+//        int m = partition(a, low, high);
+//
+//        quickSortInternally(a, low, m);
+//        quickSortInternally(a, m + 1, high);
+//
+//    }
+//
+//    public static int partition(int[] a, int low, int high) {
+//        // 切分元素
+//        int p = a[low];
+//        // 下一个小于切分元素可插入的位置
+//        int i = low;
+//
+//        for (int j = low; j < high; j++) {
+//
+//        }
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
