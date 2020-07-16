66cbb827b3b312a56919dec36029ae9f287127cd
SillyBoy007 <814752288@qq.com>
2020-07-16 19:39:33 +0800

    有效的数独

--- src/main/java/com/wxy/learn/practice/graph/leetcode/EffectiveSudoku.java
+++ src/main/java/com/wxy/learn/practice/graph/leetcode/EffectiveSudoku.java
@@ -33,9 +33,32 @@ public class EffectiveSudoku {
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    int n = Integer.valueOf(num);
+                    int boxIndex = (i / 3) * 3 + j / 3;
+                    /**
+                     * 同一行是否有相同的数字
+                     */
+                    rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
+                    /**
+                     * 同一列是否有相同的数字
+                     */
+                    cols[j].put(n, cols[j].getOrDefault(n, 0) + 1);
 
+                    /**
+                     * 3*3单元格内是否有相同的数字
+                     */
+                    boxes[boxIndex].put(n, boxes[boxIndex].getOrDefault(n, 0) + 1);
+                    if (rows[i].get(n) > 1 || cols[j].get(n) > 1 || boxes[boxIndex].get(n) > 1) {
+                        return false;
+                    }
+
+                }
             }
         }
-        return  true;
+        return true;
     }
+
+
 }
