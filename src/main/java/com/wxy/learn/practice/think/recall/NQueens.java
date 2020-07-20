e115e1f0b7d367aa4681c1be3ef4ace00e1cf50a
wangxiayun <wangxiayun@aikucun.com>
2020-07-20 20:26:14 +0800

    01背包-回溯解法

--- src/main/java/com/wxy/learn/practice/think/recall/NQueens.java
+++ src/main/java/com/wxy/learn/practice/think/recall/NQueens.java
@@ -6,10 +6,42 @@ import java.util.List;
 
 /**
  * 回溯算法解题N皇后
- *
+ * 参考 https://blog.csdn.net/folkboat/article/details/103308926
  * @author wangxiayun
  * @since 2020/7/20
  **/
+
+
+//回溯解题框架
+//    void backtrack(resultStack, arg1, arg2, ...){
+//        // 当满足题目的某种要求时，说明此时找到了问题的一个解
+//        if (some condition){
+//            // 在这里根据题目要求做出相关操作
+//            // 这里的操作一般是将resultStack复制一份，加入到所有解的集合中
+//            some operations;
+//
+//            // 直接返回
+//            return;
+//        }
+//
+//        // candidates 表示"要求2"中提到的多种候选决策
+//        // 在这里通过for循环遍历所有的候选决策
+//        for (candidate : candidates){
+//            // 当候选条件不符合要求时，跳过这个条件
+//            if (candidate does not satisfy some requirements){
+//                continue;
+//            }
+//
+//            // 下面三行是这个函数的核心，体现了回溯算法能进则进的思想
+//            // 先把当前候选条件加入结果栈中，然后进入下一步，
+//            // 从下一步中返回后，将当前候选条件出栈，把结果栈恢复到之前的状态，
+//            // 以便考虑下一个candidate（for循环的下一个元素）
+//            resultStack.push(candidate);
+//            backtrack(resultStack, arg1, arg2);
+//            resultStack.pop();
+//        }
+//    }
+
 public class NQueens {
     /***
      * 棋盘
