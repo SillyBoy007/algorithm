3e9fecc046a2eccf895b1422f6ff62843969872d
wangxiayun <814752288@qq.com>
2021-01-29 18:49:00 +0800

    fix

---
+++ src/main/java/com/wxy/learn/daily/y2021/m01/day28/FindPivotIndex.java
@@ -0,0 +1,32 @@
+package com.wxy.learn.daily.y2021.m01.day28;
+
+
+import java.util.Arrays;
+
+/**
+ * 724.寻找数组的中心索引
+ * 给定一个整数类型的数组 nums，请编写一个能够返回数组 “中心索引” 的方法。
+ * 我们是这样定义数组 中心索引 的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+ * 如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/find-pivot-index
+ *
+ * @author wxy
+ * @since 2021-01-28
+ */
+public class FindPivotIndex {
+
+    public int pivotIndex(int[] nums) {
+        int total = Arrays.stream(nums).sum();
+        int sum = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (2 * sum + nums[i] == total) {
+                return i;
+            }
+            sum += nums[i];
+        }
+        return -1;
+    }
+
+
+}
